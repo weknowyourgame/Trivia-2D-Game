09211aefd08a665d3a432cbdb3951d2278f5522f
varun chaaras <chaarasvarun@gmail.com>
2025-11-20 23:53:25 +0530

    assets

---
+++ game-client/public/Tileset_Dungeon.tsx
@@ -0,0 +1,237 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tileset_Dungeon" tilewidth="16" tileheight="16" tilecount="286" columns="26">
+ <properties>
+  <property name="collision" type="bool" value="true"/>
+ </properties>
+ <image source="../../../Fortnite/Tileset_Dungeon.png" width="416" height="176"/>
+ <tile id="105">
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="106">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="107">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="108">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="131">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="132">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="133">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="134">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="157">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="9.07747" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="160">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6.97094" y="0" width="9.02906" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="183">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="9.07747" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="186">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6.97094" y="0" width="9.02906" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="209">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="210">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="211">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="212">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="235">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="236">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="237">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="238">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="260">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="261">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="262">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="263">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="264">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="265">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="266">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="267">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="268">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="269">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="270">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="271">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="272">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="273">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="274">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="275">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="276">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="277">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="278">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="279">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="280">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="281">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="282">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="283">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="284">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="285">
+  <properties>
+   <property name="col" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
